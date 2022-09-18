42507520df266fef01f7903642cb46ef1e8ed66e
Headwall Forest <headwallforest27@gmail.com>
2022-09-18 17:51:55 -0400

    Adding notes works, viewing notes and deleting doesn't. Problem with starter code?

--- Develop/routes/db.js
+++ Develop/routes/db.js
@@ -1,5 +1,9 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile
+} = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 // GET Route for retrieving all the tips
@@ -7,16 +11,31 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+notes.get('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/tips.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No note with that ID');
+    });
+});
+
 // POST Route for a new UX/UI tip
 notes.post('/', (req, res) => {
   console.log(req.body);
 
-  const { title, note } = req.body;
+  const {
+    title,
+    text
+  } = req.body;
 
   if (req.body) {
     const newNote = {
       title,
-      note,
+      text,
       note_id: uuidv4(),
     };
 
@@ -25,6 +44,22 @@ notes.post('/', (req, res) => {
   } else {
     res.error('Error in adding note');
   }
+});
+
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
 });
 
 module.exports = notes;
